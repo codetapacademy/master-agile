b678d940d1258609d69353ae92d6fab1b2b767c8
Mihaela Chetran <mihaela.chetran@yahoo.com>
2020-04-23 17:13:03 +0100

    working on adding images

--- react/src/mihaelachetran/component/block-one/block-one.component.jsx
+++ react/src/mihaelachetran/component/block-one/block-one.component.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { StyledImagesTitle, StyledMainBox, StyledImagesContainer, StyledImagesSubtitle } from "./block-one.style.js";
+// import Image from "../Image.png";
+
+import { StyledImagesTitle, StyledMainBox, StyledImagesContainer, StyledImagesSubtitle, StyledImageAvatar, StyledImageDate, StyledImageName, StyledImageIcon} from "./block-one.style.js";
 
 const BlockOne = () => {
     return (
         <>
         <StyledMainBox>
           <StyledImagesContainer>
+            <img src="{Image}" alt=""/>
             <StyledImagesTitle>Effective Forms Advertising Internet Web Site</StyledImagesTitle>
             <StyledImagesSubtitle>Comment On The Importance Of Human Life</StyledImagesSubtitle>
+              <StyledImageAvatar><img src="{avatar}" alt=""/></StyledImageAvatar>
+                <StyledImageName>Lora Stevenson</StyledImageName>
+                <StyledImageDate>Aug 15</StyledImageDate>
+              <StyledImageIcon><img src="" alt=""/></StyledImageIcon>
           </StyledImagesContainer>
         </StyledMainBox>
         </>
--- react/src/mihaelachetran/component/block-one/block-one.style.js
+++ react/src/mihaelachetran/component/block-one/block-one.style.js
@@ -1,5 +1,6 @@
 import styled from "styled-components" 
 
+
 export const StyledMainBox = styled.div`
     display: flex;
     flex-direction: column;
@@ -8,26 +9,55 @@ export const StyledMainBox = styled.div`
 
 export const StyledImagesContainer = styled.div`
     display: grid;
-    grid-template-columns: 40px 1fr 30px;
-    grid-template-rows: 1fr 46px 20px 20px;
+    grid-template-columns: 40px 1fr 40px;
+    grid-template-rows: 1fr 20px 20px 20px;
     grid-gap: 10px;
-    padding: 20px;
+    padding: 30px;
     margin-bottom: 1px;
     margin: 10px 0;
     background-size: cover;
     background-position: center;
-    background-image: url();
+    background-color:turquoise;
+    width: 540px;
+    height: 330px;
+    background-image: url(../Image.png);
 `
 
-export const StyledImagesTitle = styled.h1`
-    color: #FFFFFF;
-    grid-columns: 2/-1;
-    align-self: center;
-    padding-bottom: 1px;
+export const StyledImagesTitle = styled.div`
+    color: black;
+    grid-column:2/4;
+    // grid-row: 1/1;
+    color: white;
+    // margin-bottom: 1px;
+    align-self: end;
+`
+export const StyledImagesSubtitle = styled.div`
+    color: black;
+    grid-row: 2/3;
+    grid-column: 2/4;
+    color: white;
+    // margin-top: 1px;
 `
 
-export const StyledImagesSubtitle = styled.p`
-    grid-colums: 2/-1;
-    // grid-row: 2/3;
-    font-size: 14px;
+export const StyledImageAvatar = styled.div`
+    // background-color: black;
 `
+
+export const StyledImageName = styled.div`
+    grid-row: 3/4;
+    grid-column: 2/3;
+    color: white;
+`
+
+export const StyledImageDate = styled.div`
+    grid-row: 4/5;
+    grid-column: 2/3;
+    color: white;
+`
+
+export const StyledImageIcon = styled.div`
+    width: 30px;
+    height: 30px;
+    colour: black;
+`
+
