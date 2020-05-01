c838b203ee96a5bb2a2aaecd723c64249cab8765
Cornel <filipcornelcristian@gmail.com>
2020-05-01 18:18:23 +0300

    test commit

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,30 +1,38 @@
 <script>
-	export let name;
+  import Quote from "./Quote.svelte";
+
+  export let name;
 </script>
 
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
 <main>
-	<h1>Sanatate si virtute, cin' te prinde cine te bucura: {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Sanatate si virtute, cin' te prinde cine te bucura: {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Quote />
---
+++ svelte/src/Quote.svelte
@@ -0,0 +1,55 @@
+<style>
+  .section-inner {
+    padding: 90px;
+    height: 200px;
+    display: grid;
+    grid-template-rows: repeat(4, 1fr);
+    grid-template-columns: repeat(4, 1fr);
+  }
+
+  .image-wrapper {
+    grid-row: 4/4;
+    grid-column: 2/3;
+    text-align: end;
+  }
+
+  .quote {
+    grid-row: 2/3;
+    grid-column: 2/4;
+    text-align: center;
+  }
+
+  .name {
+    grid-row: 4/5;
+    grid-column: 3/4;
+  }
+
+  .status {
+    grid-row: 3/4;
+    grid-column: 3/4;
+  }
+</style>
+
+<div class="section-outer">
+  <div class="section-inner" id="quote">
+    <div class="quote">
+      "If enough Americans share daily how they feel, even if
+      <br />
+      they're well, this portal can provide the healthcare system
+      <br />
+      with critically valuable information."
+    </div>
+
+    <div class="image-wrapper">
+      <img src="img/alexAzar.png" alt="Alex azar photo" />
+    </div>
+    <div class="name">
+      <p>Alex Azar</p>
+      <p>
+        United States Secretary
+        <br />
+        of Health and Human Services
+      </p>
+    </div>
+  </div>
+</div>
